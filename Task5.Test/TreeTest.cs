080a17637246607e9a9142fed6eeef1564496c62
Wiktoriya <Wiktoriya>
2017-04-27 12:48:06 +0300

    task5 add

---
+++ Task5.Test/TreeTest.cs
@@ -0,0 +1,199 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task5;
+
+namespace Task5.Test
+{
+    [TestFixture]
+    public class TreeTest
+    {
+        [Test]
+        public void IntTreeStn_Positive_Test()
+        {
+            BinarySearchTree<int> bin = new BinarySearchTree<int>(new int[] { 8, 3, 10, 1, 6, 4, 7, 14, 13 });
+            bin.Remove(14);
+            string str = String.Empty;
+            foreach (var i in bin)
+                str += i + ", ";
+            Assert.AreEqual("8, 3, 1, 6, 4, 7, 10, 13, ", str);
+        }
+        [Test]
+        public void IntTreeCust_Positive_Test()
+        {
+
+            BinarySearchTree<int> bin = new BinarySearchTree<int>(new int[] { 8, 3, 10, 1, 6, 4, 7, 14, 13 }, new CustmComp());
+            bin.Remove(14);
+            string str = String.Empty;
+            foreach (var i in bin)
+                str += i + ", ";
+            Assert.AreEqual("8, 10, 13, 3, 6, 7, 4, 1, ", str);
+        }
+        private class CustmComp : IComparer<int>
+        {
+            public int Compare(int x, int y)
+            {
+                if (x > y)
+                    return -1;
+                if (x < y)
+                    return 1;
+                return 0;
+            }
+        }
+        [Test]
+        public void StringTreeStn_Positive_Test()
+        {
+            BinarySearchTree<string> bin = new BinarySearchTree<string>(new string[] { "a", "b", "c", "d", "e", "x", "d", "z", "c" });
+            bin.Remove("14");
+            string str = String.Empty;
+            foreach (var i in bin)
+                str += i ;
+            Assert.AreEqual("abcdexz", str);
+        }
+        [Test]
+        public void StrTreeCust_Positive_Test()
+        {
+
+            BinarySearchTree<string> bin = new BinarySearchTree<string>(new string[] { "8", "3", "10", "1", "6", "4", "7", "14", "13" }, new CustmSTRComp());
+            bin.Remove("14");
+            string str = String.Empty;
+            foreach (var i in bin)
+                str += i + ", ";
+            Assert.AreEqual("8, 10, 13, 3, 6, 7, 4, 1, ", str);
+        }
+        private class CustmSTRComp : IComparer<string>
+        {
+            public int Compare(string x, string y)
+            {
+                if (x == null && y == null)
+                    return 0;
+                if (x == null)
+                    return 1;
+                if (y == null)
+                    return -1;
+                CustmComp cs = new CustmComp();
+                return cs.Compare(int.Parse(x), int.Parse(y));                
+            }
+        }
+
+        [Test]
+        public void BookTreeStn_Positive_Test()
+        {
+            BinarySearchTree<Book> bin = new BinarySearchTree<Book>(new Book());
+            bin.Insert(new Book("a", 8));
+            bin.Insert(new Book("b", 3));
+            bin.Insert(new Book("c", 10));
+            bin.Insert(new Book("d", 1));
+            bin.Insert(new Book("e", 6));
+            bin.Insert(new Book("x", 4));
+            bin.Insert(new Book("g", 7));
+            bin.Insert(new Book("z", 14));
+            bin.Insert(new Book("s", 13));
+            string str = String.Empty;
+            foreach (var i in bin)
+                str += i.Cost + ", ";
+            Assert.AreEqual("8, 3, 1, 6, 4, 7, 10, 14, 13, ", str);
+        }
+        [Test]
+        public void BookTreeCust_Positive_Test()
+        {
+
+            BinarySearchTree<Book> bin = new BinarySearchTree<Book>(new BookCustmComp());
+            bin.Insert(new Book("a", 8));
+            bin.Insert(new Book("b", 3));
+            bin.Insert(new Book("c", 10));
+            bin.Insert(new Book("d", 1));
+            bin.Insert(new Book("e", 6));
+            bin.Insert(new Book("x", 4));
+            bin.Insert(new Book("g", 7));
+            bin.Insert(new Book("z", 14));
+            bin.Insert(new Book("s", 13));
+            string str = String.Empty;
+            foreach (var i in bin)
+                str += i.Cost + ", ";
+            Assert.AreEqual("8, 10, 14, 13, 3, 6, 7, 4, 1, ", str);
+        }
+        private class Book : IComparer<Book>
+        {
+            public string Name { get; set; }
+            public int Cost { get; set; }
+            public Book() { }
+            public Book(string name, int cost)
+            {
+                Name = name;
+                Cost = cost;
+            }
+
+            public int Compare(Book x, Book y)
+            {
+                if (x == null && y == null)
+                    return 0;
+                if (x == null || x.Cost < y.Cost)
+                    return -1;
+                if (y == null || x.Cost > y.Cost)
+                    return 1;
+                return 0;
+            }
+        }
+        private class BookCustmComp : IComparer<Book>
+        {
+            public int Compare(Book x, Book y)
+            {
+                if (x == null && y == null)
+                    return 0;
+                if (x == null )
+                    return 1;
+                if (y == null)
+                    return -1;
+                CustmComp cs = new CustmComp();
+                return cs.Compare(x.Cost, y.Cost);
+            }
+        }
+        [Test]
+        public void PointTreeCust_Positive_Test()
+        {
+
+            BinarySearchTree<Point> bin = new BinarySearchTree<Point>(new PointCustmComp());
+            bin.Insert(new Point(1, 8));
+            bin.Insert(new Point(1, 3));
+            bin.Insert(new Point(1, 10));
+            bin.Insert(new Point(1, 1));
+            bin.Insert(new Point(1, 6));
+            bin.Insert(new Point(1, 4));
+            bin.Insert(new Point(1, 7));
+            bin.Insert(new Point(1, 14));
+            bin.Insert(new Point(1, 13));
+            string str = String.Empty;
+            foreach (var i in bin)
+                str += i.Y + ", ";
+            Assert.AreEqual("8, 10, 14, 13, 3, 6, 7, 4, 1, ", str);
+        }
+        private struct Point 
+        {
+            public int X;
+            public int Y;
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+        private class PointCustmComp : IComparer<Point>
+        {
+            public int Compare(Point x, Point y)
+            {
+                if (x.X == 0 && x.Y == 0 && y.X == 0 && y.Y == 0)
+                    return 0;
+                if (x.X == 0 && x.Y == 0)
+                    return 1;
+                if (y.X == 0 && y.Y == 0)
+                    return -1;
+                CustmComp cs = new CustmComp();
+                return cs.Compare(x.Y, y.Y);
+            }
+        }
+    }
+}
---
+++ Task5/BinarySearchTree.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    public class BinarySearchTree<T> : IEnumerable<T>
+    {
+        private IComparer<T> comparer;
+        public T Data { get; private set; }
+        public BinarySearchTree<T> Left { get; set; }
+        public BinarySearchTree<T> Right { get; set; }
+        public BinarySearchTree<T> Parent { get; set; }
+        #region ctor
+        public BinarySearchTree()
+        {
+            this.comparer = Comparer<T>.Default;
+        }
+        public BinarySearchTree(IEnumerable<T> collection)
+        {
+            this.comparer = Comparer<T>.Default;
+            foreach (var item in collection)
+                Insert(item);
+        }
+        public BinarySearchTree(IComparer<T> comparer)
+        {
+            this.comparer = comparer;
+        }
+        public BinarySearchTree(IEnumerable<T> collection, IComparer<T> comparer)
+        {
+            this.comparer = comparer;
+            foreach (var item in collection)
+                Insert(item);
+        }
+        #endregion
+        #region public methods
+        /// <summary>
+        ///  The method inserts value into tree
+        /// </summary>
+        /// <param name="data"> value</param>
+        public void Insert(T data)
+        {
+            if (ReferenceEquals(null, data)) throw new ArgumentException("data");
+            int compareResult = comparer.Compare(Data, data);
+            if (compareResult == 0 || Equals(Data, default(T)))
+            {
+                Data = data;
+                return;
+            }
+            if (compareResult > 0)
+            {
+                if (Left == null) Left = new BinarySearchTree<T>();
+                Insert(data, Left, this);
+            }
+            else
+            {
+                if (Right == null) Right = new BinarySearchTree<T>();
+                Insert(data, Right, this);
+            }
+        }
+
+        /// <summary>
+        /// The method finds value in tree
+        /// </summary>
+        /// <param name="data">input value</param>
+        /// <returns>Seeking value</returns>
+        public BinarySearchTree<T> Find(T data)
+        {
+            if (ReferenceEquals(null, data)) throw new ArgumentException("data");
+            int compareResult = comparer.Compare(Data, data);
+            if (compareResult == 0) return this;
+            if (compareResult > 0)
+            {
+                return Find(data, Left);
+            }
+            return Find(data, Right);
+        }
+        public BinarySearchTree<T> Find(T data, BinarySearchTree<T> node)
+        {
+            if (ReferenceEquals(null, node)) return null;
+            int compareResult = comparer.Compare(node.Data, data);
+            if (compareResult == 0) return node;
+            if (compareResult > 0)
+            {
+                return Find(data, node.Left);
+            }
+            return Find(data, node.Right);
+        }
+
+        /// <summary>
+        /// The method removes value from tree
+        /// </summary>
+        /// <param name="data">input value</param>
+        public void Remove(T data)
+        {
+            var removeNode = Find(data);
+            if (removeNode != null)
+            {
+                Remove(removeNode);
+            }
+        }
+        public void Remove(BinarySearchTree<T> node)
+        {
+            if (node == null) return;
+            if (node.Left == null && node.Right == null)
+            {
+                if (node.Parent.Left == node) node.Parent.Left = null;
+                else node.Parent.Right = null;
+                return;
+            }
+            if (node.Left == null && !ReferenceEquals(null, node.Parent))
+            {
+                if (node.Parent.Left == node) node.Parent.Left = node.Right;
+                else node.Parent.Right = node.Right;
+                node.Right.Parent = node.Parent;
+                return;
+            }
+            if (node.Right == null)
+            {
+                if (node.Parent.Left == node) node.Parent.Left = node.Left;
+                else node.Parent.Right = node.Left;
+                node.Left.Parent = node.Parent;
+                return;
+            }
+
+            if (node.Parent == null)
+            {
+                var bufLeft = node.Left;
+                var bufRightLeft = node.Right.Left;
+                var bufRightRight = node.Right.Right;
+                node.Data = node.Right.Data;
+                node.Right = bufRightRight;
+                node.Left = bufRightLeft;
+                if (!ReferenceEquals(null, bufLeft))
+                    Insert(bufLeft, node, node);
+                return;
+            }
+
+            if (node == node.Parent.Left)
+            {
+                node.Parent.Left = node.Right;
+            }
+            if (node == node.Parent.Right)
+            {
+                node.Parent.Right = node.Right;
+            }
+            else
+            {
+                node.Right.Parent = node.Parent;
+                Insert(node.Left, node.Right, node.Right);
+            }
+        }
+
+        public IEnumerable<T> Preorder()
+        {
+            BinarySearchTree<T> node = this;
+            yield return this.Data;
+            while (true)
+            {
+                if (!ReferenceEquals(null, node.Left))
+                {
+                    node = node.Left;
+                    yield return node.Data;
+                }
+                else if (!ReferenceEquals(null, node.Right))
+                {
+                    node = node.Right;
+                    yield return node.Data;
+                }
+                else
+                {
+                    if (node.Parent == null) yield break;
+                    if (node == node.Parent.Left)
+                    {
+                        node = node.Parent;
+                        node.Left = null;
+                    }
+                    else
+                    {
+                        node = node.Parent;
+                        node.Right = null;
+                    }
+                }
+
+            }
+
+        }
+
+        public IEnumerable<T> Inorder()
+        {
+            BinarySearchTree<T> lastNode = null;
+            BinarySearchTree<T> node = this;
+            while (node != null)
+            {
+                if (lastNode == node.Parent)
+                {
+                    if (node.Left != null)
+                    {
+                        lastNode = node;
+                        node = node.Left;
+                        continue;
+                    }
+                    else
+                        lastNode = null;
+                }
+                if (lastNode == node.Left)
+                {
+                    yield return node.Data;
+
+                    if (node.Right != null)
+                    {
+                        lastNode = node;
+                        node = node.Right;
+                        continue;
+                    }
+                    else
+                        lastNode = null;
+                }
+                if (lastNode == node.Right)
+                {
+                    lastNode = node;
+                    node = node.Parent;
+                }
+            }
+        }
+
+        public IEnumerable<T> Postorder()
+        {
+            BinarySearchTree<T> node = this;
+            while (true)
+            {
+                if (!ReferenceEquals(null, node.Left))
+                {
+                    node = node.Left;
+                }
+                else if (!ReferenceEquals(null, node.Right))
+                {
+                    node = node.Right;
+                }
+                else
+                {
+                    yield return node.Data;
+                    if (node.Parent == null) yield break;
+                    if (node == node.Parent.Left)
+                    {
+                        node = node.Parent;
+                        node.Left = null;
+                    }
+                    else
+                    {
+                        node = node.Parent;
+                        node.Right = null;
+                    }
+                }
+
+            }
+
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Preorder().GetEnumerator();
+        }
+        #endregion
+        #region prvate methods
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private void Insert(T data, BinarySearchTree<T> node, BinarySearchTree<T> parent)
+        {
+            int compareResult = comparer.Compare(node.Data, data);
+            if (compareResult == 0 || Equals(node.Data, default(T)))
+            {
+                node.Data = data;
+                node.Parent = parent;
+                return;
+            }
+
+            if (compareResult > 0)
+            {
+                if (node.Left == null) node.Left = new BinarySearchTree<T>();
+                Insert(data, node.Left, node);
+            }
+            else
+            {
+                if (node.Right == null) node.Right = new BinarySearchTree<T>();
+                Insert(data, node.Right, node);
+            }
+
+        }
+        /// <summary>
+        ///  The method inserts node into tree
+        /// </summary>
+        /// <param name="data">node</param>
+        private void Insert(BinarySearchTree<T> data, BinarySearchTree<T> node, BinarySearchTree<T> parent)
+        {
+            int compareResult = comparer.Compare(node.Data, data.Data);
+            if (node.Data.Equals(default(T)) || compareResult == 0)
+            {
+                node.Data = data.Data;
+                node.Left = data.Left;
+                node.Right = data.Right;
+                node.Parent = parent;
+                return;
+            }
+            if (compareResult > 0)
+            {
+                if (node.Left == null) node.Left = new BinarySearchTree<T>();
+                Insert(data, node.Left, node);
+            }
+            else
+            {
+                if (node.Right == null) node.Right = new BinarySearchTree<T>();
+                Insert(data, node.Right, node);
+            }
+        }
+        #endregion
+    }
+}
